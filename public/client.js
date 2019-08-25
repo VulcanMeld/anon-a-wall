8537906e05e64d5f12686d6d1a7cbc04ec67b3db
Christopher <chrisformey@yahoo.com>
2019-08-25 15:24:21 -0500

    fix errors and formatting of client

--- public/client.js
+++ public/client.js
@@ -1,38 +1,49 @@
-'use strict'
+"use strict";
 
-const wallPosts = [ 
-   {"content": "hello, I love you all",
-   "selfDestruct": { "on": true, "timeInMinutes":30}
+const wallPosts = [
+  {
+    content: "hello, I love you all",
+    selfDestruct: { on: true, timeInMinutes: 30 },
+    likes: 0
   },
- 
- {"content": "hello, I love you all",
-   "selfDestruct": { "on": true, "timeInMinutes":30}
- }
-]
 
-const getWallPosts = (callBackFn) => {
-	callBackFn(wallPosts)
-	
-	
-	
-	
-	}
-	
-	const watchForm =() => {
-		$(".js-user-text-box").on("submit", function(event) {
-			event.preventDefault()
-			
+  {
+    content: "hello, I love you all",
+    selfDestruct: { on: true, timeInMinutes: 30 },
+    likes: 0
+  }
+];
 
-}
-		
-		}
+const getWallPosts = callBackFn => {
+  let wallPostsData = wallPosts;
+  displayWallPosts(wallPostsData);
+};
 
+const watchForm = () => {
+  $(".js-user-text-box").on("submit", function(event) {
+    event.preventDefault();
+  });
+};
 
+const displayWallPosts = data => {
+  for (i = 0; i < data.length; i++) {
+    $(".js-wall").append(
+      `<div class = 'post' id = ${i}><p>${data[i].content}</p><button class = "js-like-button"></button><p class = "js-like-count">Like Count: ${data}[i].likes </p></div>`
+    );
+  }
+
+  $(".js-like-button").on("click", function(event) {
+    getLikes;
+  });
+
+  const getLikes = callBackFn => {};
+};
+
+const displayLikes = () => {};
 
 const startApp = () => {
-	getWallPosts()
-	watchForm(displayWallPosts)
-	
-	}
-	
-$(startApp)
+  getWallPosts(displayWallPosts);
+  watchForm();
+};
+
+$(startApp);
